dd92dda5cb197517e542562bddca1022ffe58396
Naga Nandini Mummidivarapu <nanduu2510@gmail.com>
2025-07-26 17:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.93%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,12 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         res={}
-        def fib(n,res):
+        def fib(n):
             if n==0:
                 return 0
             if n<=2:
                 return 1
             if n not in res:
-                res[n]=fib(n-1,res)+fib(n-2,res)+fib(n-3,res)
+                res[n]=fib(n-1)+fib(n-2)+fib(n-3)
             return res[n]
-        return fib(n,res)
+        return fib(n)
