172cbbc60fe494ef9077cc2842ea80c55ee743b2
Naga Nandini Mummidivarapu <nanduu2510@gmail.com>
2025-07-28 19:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.79%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
@@ -1,7 +1,12 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        stack=[]
-        for i in range(len(hours)):
-            if hours[i]>=target:
-                stack.append(hours[i])
-        return len(stack)
+        # stack=[]
+        # for i in range(len(hours)):
+        #     if hours[i]>=target:
+        #         stack.append(hours[i])
+        # return len(stack)
+        count=0
+        for i in hours:
+            if i>=target:
+                count+=1
+        return count
