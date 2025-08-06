a51383089190349004e636451b6ea9d75bd494bf
Naga Nandini Mummidivarapu <nanduu2510@gmail.com>
2025-08-06 20:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.78%) - LeetSync

--- 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.py
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.py
@@ -1,4 +1,3 @@
 class Solution:
     def findDelayedArrivalTime(self, arrivalTime: int, delayedTime: int) -> int:
-        total = arrivalTime + delayedTime
-        return total%24
+        return (arrivalTime + delayedTime ) % 24
