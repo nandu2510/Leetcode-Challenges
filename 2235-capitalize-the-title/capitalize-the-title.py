3ea7ab253426940b4f38a216703b952e77d0b81d
Naga Nandini Mummidivarapu <nanduu2510@gmail.com>
2025-08-20 10:08:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.07%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.py
@@ -0,0 +1,14 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        words = title.split()
+        result = []
+
+        for word in words:
+            if len(word) <= 2:
+                result.append(word.lower())
+            else:
+                result.append(word.capitalize()) 
+                
+        return ' '.join(result)
+
+        
