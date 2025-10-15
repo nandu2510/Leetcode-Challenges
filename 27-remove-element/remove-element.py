1e080000d75fd6b3fdb48e052de651a2ee087d2d
Naga Nandini Mummidivarapu <nanduu2510@gmail.com>
2025-10-15 19:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.21%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -2,15 +2,9 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # k = 0  # pointer for the next position to keep
-        # for i in range(len(nums)):
-        #     if nums[i] != val:
-        #         nums[k] = nums[i]
-        #         k += 1
-        # return k
-        k=0
+        k = 0  # pointer for the next position to keep
         for i in range(len(nums)):
-            if nums[i]!=val:
-                nums[k]=nums[i]
-                k+=1
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
         return k
