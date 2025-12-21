5060599de6509ec1296fd8097a3afb9f4fff93d4
Naga Nandini Mummidivarapu <nanduu2510@gmail.com>
2025-12-21 15:32:56 +0530

    Time: 19 ms (33.80%) | Memory: 93.7 MB (22.19%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,26 @@
-import java.util.Arrays;
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-       Arrays.sort(nums);
-       int n=nums.length;
-       for(int i=0;i<n-1;i++){
-         if(nums[i]==nums[i+1]){
-            return true;
-         }
-       } 
-       return false;
-    }
+// import java.util.Arrays;
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//        int n=nums.length;
+//        for(int i=0;i<n-1;i++){
+//          if(nums[i]==nums[i+1]){
+//             return true;
+//          }
+//        } 
+//        return false;
+//     }
+// }
+
+class Solution{
+     public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hset=new HashSet<>();
+        for(int num:nums){
+            if(hset.contains(num)){
+                return true;
+            }
+            hset.add(num);
+        }
+        return false;
+     }
 }
